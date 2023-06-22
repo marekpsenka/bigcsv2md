8d1c9b9710dfbf8e29e6c678ff64cb3f161e341b
Marek Pšenka <psenka@edhouse.cz>
2023-06-22 20:53:30 +0200

    Reading csv file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use clap::Parser;
+use anyhow::Result;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short = 'o')]
     output: String,
-
     input: String,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
-    println!("{:?}", args)
+    let mut reader = csv::Reader::from_path(args.input)?;
+    for rec_result in reader.records() {
+        let rec = rec_result?;
+        println!("{:?}", rec);
+    }
+    Ok(())
 }
