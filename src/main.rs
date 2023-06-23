8985275ed2ac923feba91ba4952186f993e528eb
Marek Pšenka <psenka@edhouse.cz>
2023-06-23 20:35:13 +0200

    Headers in to_md_tables_csplit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Result};
 use clap::Parser;
 use itertools::Itertools;
+use std::fmt::Display;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -33,6 +34,16 @@ impl CsplitGuides {
     }
 }
 
+fn join_with_bars<T>(mut iter: impl Iterator<Item = T>) -> String
+where
+    T: Display,
+{
+    let mut joined = iter.join("|");
+    joined.insert(0, '|');
+    joined.insert(joined.len(), '|');
+    joined
+}
+
 fn to_md_table_simple(records: &[csv::StringRecord]) -> Vec<String> {
     records
         .iter()
@@ -40,25 +51,35 @@ fn to_md_table_simple(records: &[csv::StringRecord]) -> Vec<String> {
         .collect()
 }
 
+fn build_hline<'a>(iter: impl Iterator<Item = &'a str>) -> String {
+    join_with_bars(iter.map(|s| "-".repeat(s.len())))
+}
+
+fn record_columns_to_md(rec: &csv::StringRecord, col_from: usize, col_to: usize) -> String {
+    let to_join = (col_from..col_to).map(|icol| {
+        let range = rec.range(icol).expect("Valid index");
+        &rec.as_slice()[range]
+    });
+    join_with_bars(to_join)
+}
+
 fn to_md_tables_csplit(
+    headers: &csv::StringRecord,
     records: &[csv::StringRecord],
-    guides: &CsplitGuides
+    guides: &CsplitGuides,
 ) -> Vec<Vec<String>> {
     (0..guides.div)
         .map(|isplit| {
-            records
+            let col_from = isplit * guides.csplit;
+            let col_to = (isplit + 1) * guides.csplit;
+            let mut this_table = vec![
+                record_columns_to_md(headers, col_from, col_to)];
+
+            this_table.extend(records
                 .iter()
-                .map(move |record| {
-                    let col_from = isplit * guides.csplit;
-                    let col_to = (isplit + 1) * guides.csplit;
-                    (col_from..col_to)
-                        .map(|icol| {
-                            let range = record.range(icol).expect("Valid index");
-                            String::from(&record.as_slice()[range])
-                        })
-                        .join("|")
-                })
-                .collect::<Vec<String>>()
+                .map(|rec| record_columns_to_md(rec, col_from, col_to))
+            );
+            this_table
         })
         .collect()
 }
@@ -69,16 +90,15 @@ fn main() -> Result<()> {
 
     let csplit = args.csplit.ok_or(anyhow!("Csplit argument not provided"))?;
 
-    let headers = reader.headers()?;
+    let headers = reader.headers()?.clone();
     let ncols = headers.len();
     let guides = CsplitGuides::new(ncols, csplit);
-    println!("{guides:?}");
 
     let records = reader
         .records()
         .collect::<csv::Result<Vec<csv::StringRecord>>>()?;
 
-    let tables = to_md_tables_csplit(&records, &guides);
+    let tables = to_md_tables_csplit(&headers, &records, &guides);
     for line in tables[0].iter() {
         println!("{line}");
     }
